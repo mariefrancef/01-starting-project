b6aebda82f6563a3d2e91cf9b6086236ca3346c4
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-28 11:42:51 +0200

    add component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
