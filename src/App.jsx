ddbd25c34d0fb997eded5602f27e6d6f96881729
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-29 15:29:32 +0200

    adding an other section

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            
+          </menu>
+        </section>
       </main>
     </div>
   );
