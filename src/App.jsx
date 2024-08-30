c37cd4e85334c4111dd4f910277f0cbfbecff0d7
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-08-30 16:05:18 +0200

    adding event to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  function handleSelect() {
+    console.log("Hello World - Selected");
+  }
+
   return (
     <div>
       <Header />
@@ -24,10 +28,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
