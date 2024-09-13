8ed313e44854bc16d1827b0252d4b4a5a4de6302
MOONALIA\Moon <mariefrance.fritz@outlook.fr>
2024-09-13 15:38:28 +0200

    Props wrapped elements

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,9 @@
-export default function Section({ title, id, children }) {
+export default function Section({ title, children, ...props }) {
+  // ...props : collect all other props (receive on Section component) and merge them into a props object
   return (
-    <section id={id}>
+    <section {...props}>
+      {/* {...props} : using it to spread some data,
+    ensures that any additional accessories will be passed to this item*/}
       <h2>{title}</h2>
       {children}
     </section>
